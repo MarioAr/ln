761996d21424f6680fc08a5466b05435ed79b50c
Mario <mariorampi@gmail.com>
2020-02-07 21:48:29 -0300

    Toma de datos

---
+++ prompt.js
@@ -0,0 +1,35 @@
+var prompt = require('prompt-sync')();
+const { init } = require('./modules/fizz')
+
+const numbers = [];
+
+let n1
+
+console.log("Ingrese numeros al array. (para salir ingrese una letra)");
+
+do {
+
+    n1 = prompt(`[${numbers.join(', ')}]: `, 'q');
+
+    n1 = parseInt(n1);
+
+    if (isNaN(n1)) break;
+
+    numbers.push(n1);
+
+    
+} while (true);
+
+if (numbers.length === 0) {
+    console.log('No hay numeros en el array :(');
+    return;
+}
+
+
+const rta = init(numbers);
+
+console.clear();
+
+console.log(numbers)
+
+console.log(`\n\t${rta}`);
