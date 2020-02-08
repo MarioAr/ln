12c5aa91fbf7277249bfe67ab2d77835aef659e7
Mario <mariorampi@gmail.com>
2020-02-08 18:36:19 -0300

    Inicio server

---
+++ public/precache-manifest.4c487310bc7cc9d5a7d3c4df6a1a80da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2932e35a52f36cb1bf60401c79874107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51740d23ca6eb382f1c6",
+    "url": "/static/css/main.8ca1062f.chunk.css"
+  },
+  {
+    "revision": "c6de120c61578635922e",
+    "url": "/static/js/2.82219293.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.82219293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51740d23ca6eb382f1c6",
+    "url": "/static/js/main.865a060b.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  },
+  {
+    "revision": "6d7625d3291c21baa580da74c83aa565",
+    "url": "/static/media/data.6d7625d3.jpg"
+  },
+  {
+    "revision": "4465fea3d18f6d8d158107119aaa5fd5",
+    "url": "/static/media/ln-icons.4465fea3.woff"
+  },
+  {
+    "revision": "beb802e5d100299d789bf31a1da60966",
+    "url": "/static/media/ln-logos.beb802e5.woff"
+  },
+  {
+    "revision": "71c21ca614d24fe9228041ab5cd97c2f",
+    "url": "/static/media/ln10.71c21ca6.ttf"
+  }
+]);
